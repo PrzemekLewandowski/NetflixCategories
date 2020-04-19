db83cf759a0389b93a08aa0d74c01a3a59be8a28
Przemek Lewandowski <przemolew@interia.pl>
2020-04-19 03:31:07 +0200

    initial + added search bar + footer + test data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'netflixcategories';
+  dataset = ['MDB', 'Angular', 'Bootstrap', 'Framework', 'SPA', 'React', 'Vue'];
+  title = 'Netflix Categories';
+  searchText: any;
+
 }
